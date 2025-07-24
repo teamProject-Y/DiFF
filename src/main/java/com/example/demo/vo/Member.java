b614e2eb3a1954411ae09fe9b805d16bbd1e2094
yeji-star <sizuku134@gmail.com>
2025-07-24 21:43:14 +0900

    Revert "JWT 해보는 중"

--- src/main/java/com/example/demo/vo/Member.java
+++ src/main/java/com/example/demo/vo/Member.java
@@ -2,46 +2,37 @@ package com.example.demo.vo;
 
 import java.time.LocalDateTime;
 
-import com.example.demo.domain.Role;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Member {
 
-    private Long id;
+    public Member(String loginId, String loginPw, String name, String nickName, String email) {
+        this.loginId = loginId;
+        this.loginPw = loginPw;
+        this.name = name;
+        this.nickName = nickName;
+        //this.cellPhone = cellPhone;
+        this.email = email;
+    }
 
+    private long id;
     private String oauthId;
-
     private LocalDateTime regDate;
-    private LocalDateTime updateDate;
-
     private String loginId;
     private String loginPw;
     private String name;
     private String nickName;
+    //private String cellPhone;
     private String email;
-
     private boolean delStatus;
     private LocalDateTime delDate;
 
     private boolean verified;
-
-    private Role role;
-    private String contect;
-
-    // OAuth 로그인 정보로부터 새 Member vo 생성 ? (기본 ROLE_USER 설정)
-    public static Member fromOAuthLogin(String oauthId, String email, String nickName) {
-        return Member.builder()
-                .oauthId(oauthId)
-                .email(email)
-                .nickName(nickName)
-                .role(Role.ROLE_USER)
-                .build();
-    }
-
 }
