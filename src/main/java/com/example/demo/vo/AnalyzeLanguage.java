7aa55199c9ce3da3ab6362b69308b4d0cee8e6e5
Yullc <apanan0910@gmail.com>
2025-08-01 11:07:34 +0900

    회원가입 수정 , 분석 vo 추가

---
+++ src/main/java/com/example/demo/vo/AnalyzeLanguage.java
@@ -0,0 +1,17 @@
+package com.example.demo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnalyzeLanguage {
+    private Long id;
+    private Long analyzeId;
+    private String language;
+    private int lines;
+}
