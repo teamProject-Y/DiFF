0d72a62df46c6de2737e2340631d7446ee0306f8
yunzivv <yunzivv@gmail.com>
2025-08-22 12:33:06 +0900

    로컬, 소셜 로그인 통합

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -1,55 +1,45 @@
 package com.example.demo.repository;
 
-import com.example.demo.vo.Follow;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.demo.vo.Member;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface MemberRepository {
 
-    // 지울 거
-    public int doJoin(String loginId, String loginPw, String name, String nickName, String email);
+    public int join(@Param("loginPw") String loginPw,
+                    @Param("nickName") String nickName,
+                    @Param("email") String email);
 
-    // 남길 거
-    public int join(String loginPw, String nickName, String email);
+    public Member getByOauthIdAndProvider(@Param("oauthId") String oauthId,
+                                          @Param("provider") String provider);
 
-    // 지울 거
-    public int isExistsNameNEmail(String name, String email);
+    public void uploadProfileImg(@Param("memberId") Long memberId,
+                                 @Param("profileUrl") String profileUrl);
 
-    // 남길 거
+    public int modifyMember(@Param("loginedMemberId") long loginedMemberId,
+                            @Param("loginId") String loginId,
+                            @Param("loginPw") String loginPw,
+                            @Param("name") String name,
+                            @Param("nickName") String nickName,
+                            @Param("email") String email);
+
     public int isExistsEmail(String email);
-
-    public void doLogin(int id);
 
     public Long getLastInsertId();
 
     public Member getMemberById(Long id);
 
-    // 지울 거
-    public Member getMemberByLoginId(String loginId);
-
     public Member getMemberByEmail(String email);
-
-    public int modifyMember(long loginedMemberId, String loginId, String loginPw, String name, String nickName, String email);
-
-    public Member getByOauthId(String oauthId);
-
-    public void save(Member member);
 
     public void saveMember(Member member);
 
     public Member getById(Long memberId);
 
-    public Member getByOauthIdAndProvider(String oauthId, String provider);
-
-    public List<Follow> getFollowsByMemberId(Long memberId);
-
     public List<Member> getFollowingList(Long memberId);
 
     public Member getMemberByNickName(String nickName);
-
-    public void uploadProfileImg(Long memberId, String profileUrl);
 }
