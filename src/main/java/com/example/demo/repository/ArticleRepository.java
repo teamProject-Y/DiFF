7e5289cebfb3112dbaee23e16b585918b68e1f1d
yunzivv <yunzivv@gmail.com>
2025-08-26 23:35:36 +0900

    article error fiz

--- src/main/java/com/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -20,7 +20,7 @@ public interface ArticleRepository {
 
     public int writeArticle(Long memberId, String title, String body, String checksum, Long repositoryId);
 
-    public int writeArticle(Long memberId, String title, String body, String checksum, Long repositoryId, Long draftId);
+    // public int writeArticle(Long memberId, String title, String body, String checksum, Long repositoryId, Long draftId);
 
     public Article getArticleById(Long id);
 
