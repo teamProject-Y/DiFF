2cfee346687aadaac795a6bf54d7a164d3677606
Yullc <apanan0910@gmail.com>
2025-08-26 23:30:47 +0900

    수정

--- src/main/java/com/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -18,7 +18,7 @@ public interface ArticleRepository {
 
     public List<Article> getTrendingArticles(Integer count, Integer days);
 
-    public int writeArticle(Long memberId, String title, String body, String checksum, Long repositoryId, Long draftId);
+    public int writeArticle(Long memberId, String title, String body, String checksum, Long repositoryId);
 
     public Article getArticleById(Long id);
 
