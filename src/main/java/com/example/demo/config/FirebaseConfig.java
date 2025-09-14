37e1fb8b24161a96c5e7a01d2b6b02437464ef6f
Yullc <apanan0910@gmail.com>
2025-09-14 22:32:16 +0900

    remove .env from git history

--- src/main/java/com/example/demo/config/FirebaseConfig.java
+++ src/main/java/com/example/demo/config/FirebaseConfig.java
@@ -4,30 +4,31 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
 
     @Bean
-    public FirebaseApp firebaseApp() throws IOException {
-        if (FirebaseApp.getApps().isEmpty()) {
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/firebase/serviceAccountKey.json");
-
+    public FirebaseApp firebaseApp(@Value("${fcm.key}") String firebaseKey) throws IOException {
+        try (InputStream serviceAccount =
+                     new ByteArrayInputStream(firebaseKey.getBytes(StandardCharsets.UTF_8))) {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
             FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(credentials)
                     .build();
-
             return FirebaseApp.initializeApp(options);
-        } else {
-            return FirebaseApp.getInstance();
         }
     }
+
 
     @Bean
     public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
