d27a3ca462cbc64027c5d4aabbefadf02e8aa456
Yullc <apanan0910@gmail.com>
2025-09-14 22:49:20 +0900

    re

--- src/main/java/com/example/demo/config/FirebaseConfig.java
+++ src/main/java/com/example/demo/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
