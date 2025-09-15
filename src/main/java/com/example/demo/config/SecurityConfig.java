edd9650454450cab6419d64cf6b383b6f37ae04c
yunzivv <yunzivv@gmail.com>
2025-09-15 11:10:30 +0900

    web config fix 2

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -49,6 +49,9 @@ public class SecurityConfig {
                 .httpBasic(hb -> hb.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers("/actuator/health", "/actuator/health/**", "/actuator/info").permitAll()
+
                         .requestMatchers("/error").permitAll()
 
                         // 완전 공개 (누구나 접근 가능)
@@ -89,7 +92,6 @@ public class SecurityConfig {
                         ).permitAll()
 
                         // 그 외는 인증 필요
-
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
@@ -113,7 +115,8 @@ public class SecurityConfig {
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("http://diff.io.kr:3000/DiFF/home/main")
+                        .logoutSuccessUrl("https://diff-front.fly.dev/DiFF/home/main")
+//                        .logoutSuccessUrl("http://diff.io.kr:3000/DiFF/home/main")
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
                 );
@@ -144,13 +147,18 @@ public class SecurityConfig {
         cfg.setAllowCredentials(true);
 
         // 허용할 프론트 주소들
-        cfg.addAllowedOrigin("http://13.124.33.233:3000");
-        cfg.addAllowedOrigin("http://diff.io.kr:3000");// EC2 프론트
-        cfg.addAllowedOrigin("http://localhost:3000");     // 로컬 개발
-        cfg.addAllowedOrigin("http://127.0.0.1:3000");     // 로컬 개발
+        cfg.addAllowedOrigin("https://diff-front.fly.dev");
+        cfg.addAllowedOrigin("https://diff.io.kr");
 
-        // 테스트 시 전체 허용 (필요하면 주석 풀고 테스트)
-        // cfg.addAllowedOriginPattern("*");
+        // 로컬 개발용
+        cfg.addAllowedOrigin("http://localhost:3000");
+        cfg.addAllowedOrigin("http://127.0.0.1:3000");
+
+        // 여러 fly.dev 서브도메인 테스트할 때
+        cfg.addAllowedOriginPattern("https://*.fly.dev");
+
+        // 테스트 시 전체 허용
+        cfg.addAllowedOriginPattern("*");
 
         cfg.addAllowedHeader("*");
         cfg.addAllowedMethod("*");
