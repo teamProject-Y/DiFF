5c571633244874bdf6ab7c6d25f85d9ffe029670
yunzivv <yunzivv@gmail.com>
2025-09-16 11:46:15 +0900

    interceptor resurrection

--- src/main/java/com/example/demo/WebMvcConfigurer.java
+++ src/main/java/com/example/demo/WebMvcConfigurer.java
@@ -1,65 +1,67 @@
-//package com.example.demo;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//
-//import com.example.demo.interceptor.BeforeActionInterceptor;
-//import com.example.demo.interceptor.NeedLoginInterceptor;
-//import com.example.demo.interceptor.NeedLogoutInterceptor;
-//
-//@Configuration
-//public class WebMvcConfigurer implements org.springframework.web.servlet.config.annotation.WebMvcConfigurer {
-//
-//    @Autowired
-//    BeforeActionInterceptor beforeActionInterceptor;
-//
-//    @Autowired
-//    NeedLoginInterceptor needLoginInterceptor;
-//
-//    @Autowired
-//    NeedLogoutInterceptor needLogoutInterceptor;
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        InterceptorRegistration ir;
-//
-//        // 로그인된 사용자 정보 세팅용
-//        ir = registry.addInterceptor(beforeActionInterceptor);
-//        ir.addPathPatterns("/**");
-//        ir.excludePathPatterns("/resource/**");
-//        ir.excludePathPatterns("/error");
-//        ir.excludePathPatterns("/favicon.ico");
-//
-//        // 로그인 필요한 요청
-//        ir = registry.addInterceptor(needLoginInterceptor);
-//        ir.addPathPatterns("/DiFF/article/write");
-//        ir.addPathPatterns("/DiFF/article/doWrite");
-//        ir.addPathPatterns("/DiFF/article/modify");
-//        ir.addPathPatterns("/DiFF/article/doModify");
-//        ir.addPathPatterns("/DiFF/article/doDelete");
-//        ir.addPathPatterns("/DiFF/member/doLogout");
-//        ir.addPathPatterns("/DiFF/reply/doWrite");
-//        ir.addPathPatterns("/DiFF/reactionPoint/doGoodReaction");
-//        ir.addPathPatterns("/DiFF/reactionPoint/doBadReaction");
-//
-//        // 로그인 상태에서는 접근 금지
-//        ir = registry.addInterceptor(needLogoutInterceptor);
-//        ir.addPathPatterns("/DiFF/member/login");
-//        ir.addPathPatterns("/DiFF/member/doLogin");
-//        ir.addPathPatterns("/DiFF/member/join");
-//        ir.addPathPatterns("/DiFF/member/doJoin");
-//    }
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:3000/") // 프론트 주소
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .exposedHeaders("*")
-//                .allowCredentials(true); // 필요하다면
-//    }
-//}
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+
+import com.example.demo.interceptor.BeforeActionInterceptor;
+import com.example.demo.interceptor.NeedLoginInterceptor;
+import com.example.demo.interceptor.NeedLogoutInterceptor;
+
+@Configuration
+public class WebMvcConfigurer implements org.springframework.web.servlet.config.annotation.WebMvcConfigurer {
+
+    @Autowired
+    BeforeActionInterceptor beforeActionInterceptor;
+
+    @Autowired
+    NeedLoginInterceptor needLoginInterceptor;
+
+    @Autowired
+    NeedLogoutInterceptor needLogoutInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration ir;
+
+        // 로그인된 사용자 정보 세팅용
+        ir = registry.addInterceptor(beforeActionInterceptor);
+        ir.addPathPatterns("/**");
+        ir.excludePathPatterns("/resource/**");
+        ir.excludePathPatterns("/error");
+        ir.excludePathPatterns("/favicon.ico");
+
+        // 로그인 필요한 요청
+        ir = registry.addInterceptor(needLoginInterceptor);
+        ir.addPathPatterns("/DiFF/article/write");
+        ir.addPathPatterns("/DiFF/article/doWrite");
+        ir.addPathPatterns("/DiFF/article/modify");
+        ir.addPathPatterns("/DiFF/article/doModify");
+        ir.addPathPatterns("/DiFF/article/doDelete");
+        ir.addPathPatterns("/DiFF/member/doLogout");
+        ir.addPathPatterns("/DiFF/reply/doWrite");
+        ir.addPathPatterns("/DiFF/reactionPoint/doGoodReaction");
+        ir.addPathPatterns("/DiFF/reactionPoint/doBadReaction");
+
+        // 로그인 상태에서는 접근 금지
+        ir = registry.addInterceptor(needLogoutInterceptor);
+        ir.addPathPatterns("/DiFF/member/login");
+        ir.addPathPatterns("/DiFF/member/doLogin");
+        ir.addPathPatterns("/DiFF/member/join");
+        ir.addPathPatterns("/DiFF/member/doJoin");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000/") // 프론트 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(true); // 필요하다면
+    }
+
+
+}
--- src/main/java/com/example/demo/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/example/demo/interceptor/BeforeActionInterceptor.java
@@ -1,32 +1,32 @@
-//package com.example.demo.interceptor;
-//
-//import com.example.demo.service.MemberService;
-//import com.example.demo.vo.Member;
-//import com.example.demo.vo.Rq;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.servlet.HandlerInterceptor;
-//
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//
-//@Component
-//public class BeforeActionInterceptor implements HandlerInterceptor {
-//
-//    @Autowired
-//    private Rq rq;
-//
-//    @Autowired
-//    private MemberService memberService;
-//
-//    @Override
-//    public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-//
-//        if (rq.isLogined()) {
-//            Member member = memberService.getMemberById((long) rq.getLoginedMemberId());
-//            rq.setLoginedMember(member);
-//        }
-//
-//        return true;
-//    }
-//}
+package com.example.demo.interceptor;
+
+import com.example.demo.service.MemberService;
+import com.example.demo.vo.Member;
+import com.example.demo.vo.Rq;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class BeforeActionInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private Rq rq;
+
+    @Autowired
+    private MemberService memberService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+
+        if (rq.isLogined()) {
+            Member member = memberService.getMemberById((long) rq.getLoginedMemberId());
+            rq.setLoginedMember(member);
+        }
+
+        return true;
+    }
+}
