0cb747a5230061f013b56acb5fa66893bff30410
Yullc <apanan0910@gmail.com>
2025-09-23 12:04:51 +0900

    ci(deploy): setup auto-deployment with GitHub Actions and environment variables
    ci(deploy): setup auto-deployment with GitHub Actions and environment variables

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -30,20 +30,17 @@ public class UsrArticleController {
 
     @GetMapping("/list")
     public ResponseEntity<Map<String, Object>> showList( HttpServletRequest req,
-            @RequestParam(defaultValue = "repositoryId") Long repositoryId,
-            @RequestParam(defaultValue = "1") int page,
-            @RequestParam(required = false) String keyword,
-            @RequestParam(defaultValue = "0") int searchItem) {
+                                                         @RequestParam(defaultValue = "repositoryId") Long repositoryId,
+                                                         @RequestParam(defaultValue = "1") int page,
+                                                         @RequestParam(required = false) String keyword,
+                                                         @RequestParam(defaultValue = "0") int searchItem) {
 
         System.out.println("===== 📑 [Get] /api/DiFF/article/list =====");
 
         Rq rq = (Rq) req.getAttribute("rq");
         Long loginedMemberId = rq.getLoginedMemberId();
 
-        int itemsInAPage = 10;
-        int limitFrom = (page - 1) * itemsInAPage;
-
-        List<Article> articles = articleService.getArticles(repositoryId, keyword, searchItem, limitFrom, itemsInAPage, loginedMemberId);
+        List<Article> articles = articleService.getArticles(repositoryId, keyword, searchItem, loginedMemberId);
 
         Map<String, Object> result = new HashMap<>();
         result.put("articles", articles);
@@ -202,7 +199,6 @@ public class UsrArticleController {
         }
 
         int affectedRow = articleService.modifyArticle(article);
-
 
         if (affectedRow == 0) {
             return ResultData.from("F-4", "수정 실패", 0);
