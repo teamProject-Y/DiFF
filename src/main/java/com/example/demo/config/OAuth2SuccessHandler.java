756164ef077446708cbc1470e0d78c92ab0f6192
yunzivv <yunzivv@gmail.com>
2025-09-24 13:03:05 +0900

    redirect url fix

--- src/main/java/com/example/demo/config/OAuth2SuccessHandler.java
+++ src/main/java/com/example/demo/config/OAuth2SuccessHandler.java
@@ -107,7 +107,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         String accessToken = jwtTokenProvider.generateAccessToken(member.getId(), member.getNickName(), member.getEmail());
         String refreshToken = jwtTokenProvider.generateRefreshToken(member.getId(), member.getNickName(), member.getEmail());
 
-        String redirectUrl = "http://localhost:3000/DiFF/home/main"
+        String redirectUrl = "http://diff.io.kr/DiFF/home/main"
                 + "?access_token=" + accessToken
                 + "&refresh_token=" + refreshToken;
         response.sendRedirect(redirectUrl);
